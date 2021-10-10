0b5a65e2ffac8a600a94e468af9cbf60a31bd3c7
sion <parksion9262@gmail.com>
2021-10-10 21:51:49 +0900

    create-board dto 생성 및 적용

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Board } from './board.model';
 import { BoardsService } from './boards.service';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -13,11 +14,8 @@ export class BoardsController {
     }
 
     @Post()
-    createBoard(
-        @Body('title') title: string,
-        @Body('description') description: string
-    ): Board {
-        return this.boardsService.createBoard(title, description);
+    createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+        return this.boardsService.createBoard(createBoardDto);
     }
     
 }
